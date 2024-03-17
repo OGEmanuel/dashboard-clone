659104ca290758fd5b82e14f2cd904d592203701
OGEmanuel <1blacksanta416@gmail.com>
2024-03-17 09:40:28 +0100

    Initial Commit - Login UI complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,41 @@
+// import Image from "next/image";
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className={styles["auth_login"]}>
+      <Image
+        className={styles.logo}
+        src={"/logo.svg"}
+        alt="logo"
+        width={173.76}
+        height={36}
+      />
+      <div className={styles["main-box"]}>
+        <Image
+          src={"/pablo-sign-in.png"}
+          alt="sign-in"
+          width={600}
+          height={337.57}
+        />
+        <div className={styles["form-box"]}>
+          <h1>Welcome!</h1>
+          <p className={styles.info}>Enter details to login.</p>
+
+          <form action="">
+            <div className={styles["form-inputs"]}>
+              <input type="email" placeholder="Email" />
+              <span className={styles["password-input"]}>
+                <input type="passoword" placeholder="Password" />
+                <button className={styles.show}>SHOW</button>
+              </span>
+              <p>FORGOT PASSWORD?</p>
+            </div>
+            <button className={styles["login-btn"]}>LOG IN</button>
+          </form>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
 }
